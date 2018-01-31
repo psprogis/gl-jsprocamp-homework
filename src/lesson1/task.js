410296cd79f4347dabb074e035d268fc65b944d0
Pavel Sprogis <pavel.sprogis@gmail.com>
2018-01-31 18:26:17 +0200

    lesson1: add jsdoc and solutions for the last 3 tasks, add editorconfig

--- src/lesson1/task.js
+++ src/lesson1/task.js
@@ -1,46 +1,55 @@
-// TODO: add jsdoc
-
 /*
   Should accept one argument and return its type
+
+  @param {any} variable - any number, string, object, etc.
+  @returns {string} variable type
 */
 function getDataType(variable) {
   return typeof variable;
 }
 
 /*
-  Should return:
+  Returns argument's type pseudo name, possible values:
   'primitive' if argument is a primitive datatype
   'primitive-special' if it is special primitive (null)
   'object' - for simple objects
   'object-array' - for arrays
   'object-function' - for functions
+
+  @param {any} variable - return type pseudo name of this variable
+  @returns {string} type pseudo name
 */
 function getDataTypePseudoName(variable) {
   const variableType = typeof variable;
-  const primitiveTypes = ['number', 'string', 'boolean', 'symbol', 'undefined'];
+  const primitiveTypes = ['number', 'string', 'boolean', 'symbol'];
 
   // let's break all the rules and create hydra with multiple returns
 
   if (primitiveTypes.includes(variableType)) return 'primitive';
 
-  if (variable === null) return 'primitive-special';
+  if (variable === null || variableType === 'undefined') return 'primitive-special';
 
   if (Array.isArray(variable)) return 'object-array';
 
   if (typeof variable === 'function') return 'object-function';
 
-  // ok, return just typeof result
+  // ok, return just typeof result, will work for simple objects
   return typeof variable;
 }
 
 /*
-  Should return:
-  1 if arguments types and values are equal
-  0 if only arguments' values are equal
-  -1 otherwise
+  Compare values with type
+
+  @param a {any} - first value
+  @param b {any} - second value
+  @return {number} 1 if arguments types and values are equal
+        0 if only arguments' values are equal
+        -1 otherwise
 */
 function compareByType(a, b) {
-  if (typeof a === typeof b) return 1;
+
+  /* eslint-disable eqeqeq */
+  if ((typeof a === typeof b) && (a == b)) return 1;
 
   /* eslint-disable eqeqeq */
   if (a == b) return 0;
@@ -56,17 +65,20 @@ function isNumeric(value) {
 }
 
 /*
-  Should return argument's value increased by 1 if it is number
-  otherwise return -1
+  Increase number by 1
+
+  @param {number} - numeric value to increase
+  @return {number} -  argument's value increased by 1 if it is number, otherwise -1
 */
 function increase(value) {
   return isNumeric(value) ? value + 1 : -1;
 }
 
 /*
-  Accepts 1 arguments (number)
-  returns 'safe' if argument is not a NaN or Infinity
-  otherwise returns 'danger'
+  Check if argumen is 'safe' or 'danger' value
+
+  @param {any} value - any value to test type
+  @return {string} - 'safe' if argument is not a NaN or Infinity, otherwise returns 'danger'
 */
 function testForSafeNumber(value) {
   return isNumeric(value) ? 'safe' : 'danger';
@@ -75,16 +87,20 @@ function testForSafeNumber(value) {
 // Strings
 
 /*
-  Should accept 1 string argument
-  Returns array of words in string
+  Split word in string by whitespace and return array of words
+
+  @param {string} str - any string
+  @return {Array<String>} - array of words in string
 */
 function stringToArray(str) {
-  return str.split(' ');
+  return str.trim().split(' ');
 }
 
 /*
-  Shold accept 1 string argument
-  Returns part of string before comma
+  Get part of string before comma
+
+  @param {string} str - any string
+  @return {string} - part of string before comma
 */
 function getStringPart(str) {
   let commaPos = str.indexOf(',');
@@ -94,30 +110,39 @@ function getStringPart(str) {
 }
 
 /*
-  Напишите функцию, которая принимает 2 аргумента (строку и симовл),
-  и возвращает порядковый номер симовола в строе если символ встречается в строке 1 раз,
-  false в противоположном случае
+  Finds position of a symbol in a string if string contains symbol only 1 time. or returns false.
+
+  @param {string} str - input string
+  @param {string} symbol - symbol to search for
+  @return {boolean|number} - position of symbol if it was found exactly one time in string, otherwise - false
 */
 function isSingleSymbolMatch(str, symbol) {
   const indexOfSymbol = str.indexOf(symbol);
+
+  if (indexOfSymbol === -1) return false;
+
   const isUnique = indexOfSymbol === str.lastIndexOf(symbol);
 
   return isUnique ? indexOfSymbol : false;
 }
 
 /*
-  Напишите функцию, которая принимает 2 аргумента,
-  массив в разделитель[опционально],
-  и возвращает строку ввиде элементов массива c разделителями если разделитель задан
-  или строку разделенную "-" если не задан
+  Join array elements into string
+
+  @param {Array} array - array of element to join
+  @param {string} separator - separator to use with join, default is '-'
+  @return {string} joined array
 */
 function join(array, separator = '-') {
   return array.join(separator === '' ? '-' : separator);
 }
 
 /*
-  Напишите функцию, которая принимает 2 массива,
-  и возвращает один состоящий из элементов перового и второго (последовательно сначала первый потом второй)
+  Joins to arrays
+
+  @param {Array} arrA - first array
+  @param {Array} arrB - second array
+  @return {Array} - joined array
 */
 function glue(arrA, arrB) {
   // return [...arrA, ...arrB];
@@ -125,26 +150,31 @@ function glue(arrA, arrB) {
 }
 
 /*
-  Напишите функцию, которая принимает 1 массив,
-  и возвращает другой массив отсортированный от большего к меньшему
+  Sort array in descending order. Does not change order in original array
+
+  @param {Array} arr - original array
+  @return {Array} - sorted array
 */
 function order(arr) {
-  return arr.sort((a, b) => a < b);
+  return Array.from(arr).sort((a, b) => a < b);
 }
 
 /*
-  Напишите функцию, которая принимает 1 массив,
-  и возвращает другой без чисел которые меньше 0
+  Filter out negative values from array
+
+  @param {Array} - input array
+  @return {Array} - array w/o negative numbers
 */
 function removeNegative(arr) {
   return arr.filter(item => item >= 0);
 }
 
 /*
-  Напишите функцию, которая принимает 2 числовых массива,
-  и возвращает новый массив, состоящий из элементов первого но без элементов
-  которые присутствуют во втром
-  [1,2,3], [1, 3] => [2]
+  Find difference between 2 arrays
+
+  @param {Array} arrA - first array
+  @param {Array} arrB - second array
+  @return {Array} - result of arrA - arrB
 */
 function without(arrA, arrB) {
   const result = [];
@@ -158,39 +188,163 @@ function without(arrA, arrB) {
   return result;
 }
 
-/*
-  Напишите функцию, которая принимает строку,
-  содержащую выражение математической операции с двумя
-  операндами (поддерживаются 4 базовых оператора + - / *).
-  Функция вычисляет выражение и возвращает число либо NaN.
-  '12/6' => 2
-*/
-function calcExpression(expression) {
+// helper functions for calcExpression
+function readNumber(str, start = 0) {
+  // let result = '';
+  // let end = start;
+  //
+  // const validChars = '0123456789';
+  // const validFirstChar = '+-0123456789';
+  //
+  // for (let i = start; i < str.length; ++i) {
+  //
+  // }
 
+  return parseFloat(str);
 }
 
-/*
-  Напишите функцию, которая принимает строку,
-  содержащую выражение логической операции с двумя
-  операндами (поддерживаются 5 базовых операторов > < = >= <=).
-  Функция вычисляет выражение и возвращает true / false,
-  либо бросает exception в случае ошибки.
-  '100>5' => true
-*/
+function calculate(operator, operand1, operand2) {
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = operand1 + operand2;
+      break;
+    case '-':
+      result = operand1 - operand2;
+      break;
+    case '*':
+      result = operand1 * operand2;
+      break;
+    case '/':
+      result = operand1 / operand2;
+      break;
+    case '>':
+      result = operand1 > operand2;
+      break;
+    case '<':
+      result = operand1 < operand2;
+      break;
+    case '=':
+      result = operand1 === operand2;
+      break;
+    case '>=':
+      result = operand1 >= operand2;
+      break;
+    case '<=':
+      result = operand1 <= operand2;
+      break;
+    default:
+      console.warn(`Unsupported operator: ${operator}`);
+  }
+
+  return result;
+}
+
+function parseOperator(expression, supportedOperators) {
+  let operator = expression.slice(0,2);
+  if (supportedOperators.indexOf(operator) === -1) {
+    operator = expression[0];
+    if (supportedOperators.indexOf(operator) === -1) {
+      console.warn(`Unsupported operator: ${operator}`);
+      return;
+    }
+  }
+
+  return operator;
+}
+
+function __calc(expression, supportedOperators, errorArgHandler) {
+  expression = expression.trim();
+
+  const firstOperand = readNumber(expression);
+  if ( Number.isNaN(firstOperand) ) return errorArgHandler(firstOperand);
+
+  const remainingExpression = expression.split(firstOperand).pop().trim();
+
+  const operator = parseOperator(remainingExpression, supportedOperators);
+
+  const secondOperand = readNumber(remainingExpression.slice(operator.length));
+  if ( Number.isNaN(secondOperand) ) return errorArgHandler(secondOperand);
+
+  return calculate(operator, firstOperand, secondOperand);
+}
+
 function calcComparison(expression) {
+  return __calc(expression, ['>', '<', '=', '>=', '<='], () => {
+    throw new Error(`Invalid argument`);
+  })
+  // expression = expression.trim();
+  //
+  // const supportedOperators = ['>', '<', '=', '>=', '<='];
+  //
+  // const firstOperand = readNumber(expression);
+  // if ( Number.isNaN(firstOperand) ) {
+  //   throw new Error('Invalid first operand');
+  // }
+  //
+  // const remainingExpression = expression.split(firstOperand).pop().trim();
+  //
+  // const operator = parseOperator(remainingExpression, supportedOperators);
+  //
+  // const secondOperand = readNumber(remainingExpression.slice(operator.length));
+  // if ( Number.isNaN(secondOperand) ) {
+  //   throw new Error('Invalid second operand');
+  // }
+  //
+  // return calculate(operator, firstOperand, secondOperand);
+}
 
+function calcExpression(expression) {
+  return __calc(expression, ['+', '-', '*', '/'], () => {return NaN});
+  // expression = expression.trim();
+  //
+  // const supportedOperators = ['+', '-', '*', '/'];
+  //
+  // const firstOperand = readNumber(expression);
+  // if ( Number.isNaN(firstOperand) ) {
+  //   console.log('Invalid first operand');
+  //   return NaN;
+  // }
+  //
+  // const remainingExpression = expression.split(firstOperand).pop().trim();
+  //
+  // const operator = parseOperator(remainingExpression, supportedOperators);
+  //
+  // const secondOperand = readNumber(remainingExpression.slice(1));
+  //
+  // return calculate(operator, firstOperand, secondOperand);
 }
 
 /*
-  Напишите функцию, которая принимает обьект и строку,
-  содержащую выражение доступа к свойствам обьекта.
-  Функция возвращает значение запрашиваемого свойства либо
-  бросает exception в случае ошибки.
-  { a: { x: 2 }, b: 5 }, '.a.x' => 2
-  { a: 1, b: 2 }, '.c' => exception
-*/
-function evalKey(obj, expression) {
+  Evaluates property value of the object
 
+  @param {Object} obj - object to evaluate property for
+  @param {string} expression - property path with dots
+  @return {any} - property value or throw exception
+*/
+
+// let's have more fun (and problems) and use recursion
+function evalKey(obj, expression) {
+  const secondDot = expression.indexOf('.', 1);
+
+  if (secondDot === -1) {
+    if (expression[0] !== '.') throw new Error('Invalid property name, should start with "."');
+
+    const propName = expression.replace('.', '');
+    const propValue = obj[propName];
+
+    if (propValue === undefined) {
+      throw new Error(`property ${propName} is undefined`);
+    }
+
+    return propValue;
+  }
+
+  const propName = expression.slice(1, secondDot);
+  const remainingExpression = expression.slice(secondDot);
+
+  return evalKey(obj[propName], remainingExpression);
 }
 
 export default {

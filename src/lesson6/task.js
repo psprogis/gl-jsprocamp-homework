ef4b83440ed0e48d7667580a58b6dfbef7fda19c
Pavel Sprogis <pavel.sprogis@gmail.com>
2018-02-15 16:23:05 +0200

    lession6: finish first version

--- src/lesson6/task.js
+++ src/lesson6/task.js
@@ -1,27 +1,80 @@
-// с помощью Fetch API и swapi.co API получить следующие данные
+// using Fetch API and swapi.co API get the following info
 
-// Климат на любой планете по её имени
+const API_URL = 'https://swapi.co/api';
+const DEFAULT_OPTS = {
+  mode: 'cors',
+  cache: 'default',
+  method: 'GET',
+};
+const collections = {
+  PLANETS: 'planets',
+  PEOPLE: 'people',
+  STARSHIPS: 'starships',
+};
+
+/* global fetch */
+function getItemByName(collection, name, opts) {
+  const supportedCollections = ['planets', 'people', 'starships'];
+
+  if (supportedCollections.indexOf(collection) === -1) {
+    throw new Error(`Unsupported collection: ${collection}`);
+  }
+
+  // special workaround for people collection
+  /* eslint-disable  no-param-reassign */
+  if (collection === 'people') collection += '?format=json';
+
+  return fetch(`${API_URL}/${collection}`, opts)
+    .then(resp => {
+      if (resp.status !== 200) {
+        throw new Error(`Cannot fetch ${collection} info, got unexpected status ${resp.status}`);
+      }
+
+      return resp.json();
+    })
+    .then(json => {
+      if (!json.results) {
+        throw new Error('No results array found in response');
+      }
+
+      const items = json.results.filter(item => item.name === name);
+
+      if (items.length === 0) {
+        throw new Error(`Collection: ${collection}, no items found by name: ${name}`);
+      }
+
+      if (items.length > 1) {
+        console.warn(`Collection ${collection}, more than one item found, return the first one.`);
+      }
+
+      return items[0];
+    });
+}
+
+// Climate on any planet by its name
 // {planetName} – String
-const getClimate = function(planetName) {
-
+function getClimate(planetName) {
+  return getItemByName(collections.PLANETS, planetName, DEFAULT_OPTS)
+    .then(planet => planet.climate);
 }
 
-// Получить информацию (Object) о любом персонаже по имени
+// Get profile info {Object} by name
 // {name} – String
-const getProfile = function(name) {
-
+function getProfile(name) {
+  return getItemByName(collections.PEOPLE, name, DEFAULT_OPTS);
 }
 
-// Получить список пилотов (имена в виде Array of Strings) космического корабля
-// по его названию
+// Get all pilots names (array of strings of a ship by its name
 // {starshipName} - String
-const getPilots = function(starshipName) {
-
+function getPilots(starshipName) {
+  return getItemByName(collections.STARSHIPS, starshipName, DEFAULT_OPTS)
+    .then(starship => Promise.all(starship.pilots.map(pilot => fetch(pilot))))
+    .then(responses => Promise.all(responses.map(resp => resp.json())))
+    .then(pilots => pilots.map(pilot => pilot.name));
 }
-
 
 export default {
   getClimate,
   getProfile,
-  getPilots
-}
+  getPilots,
+};
